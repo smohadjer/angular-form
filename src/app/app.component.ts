79a115d247a240470f6f3dc8a503ac9d2fc046e5
Saeid Mohadjer <saeid@fastmail.fm>
2024-01-02 16:54:32 +0100

    minor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { RouterModule } from '@angular/router';
   ],
   template: `
     <main>
+      <h1>Angular 17 Reactive Form</h1>
+      <p><a href="https://github.com/smohadjer/angular-form">Code available on GitHub</a></p>
       <router-outlet></router-outlet>
     </main>
   `,
